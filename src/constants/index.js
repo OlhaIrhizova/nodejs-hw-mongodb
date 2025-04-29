61e23fec9c3d6b6983f873a6a25f8cf22242f6a7
Olha <irhizovaolha@gmail.com>
2025-04-29 20:02:20 +0300

    change gitignor

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,7 +14,9 @@ export const SMTP = {
 
   export const TEMPLATES_DIR = path.resolve("src", "templates");
 
+
   export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
+  export const UPLOAD_DIR = path.join(process.cwd(), 'uploads');
 
 
   export const CLOUDINARY = {
